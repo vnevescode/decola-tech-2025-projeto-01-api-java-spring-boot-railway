ba9fc0617194e9eaa35b35f50bf8db0fe39e74a0
Victor Neves <victorneves15@gmail.com>
2025-03-18 23:40:32 -0300

    First commit with Java Spring Boot initial configurations

---
+++ src/test/java/me/dio/java_spring_boot_railway/JavaSpringBootRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.java_spring_boot_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
